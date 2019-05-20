4ff867f107437381b8e9a69cb06a9a9c9b138419
Roman Epifanov <roane@softwave.tech>
2019-05-20 21:47:44 +0300

    added reactive forms components to template html

--- src/app/customers/customer.component.ts
+++ src/app/customers/customer.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl } from '@angular/forms';
+import { FormGroup, FormBuilder } from '@angular/forms';
 
 import { Customer } from './customer';
 
@@ -9,22 +9,32 @@ import { Customer } from './customer';
   styleUrls: ['./customer.component.css']
 })
 export class CustomerComponent implements OnInit {
-  customerFrom: FormGroup;
+  customerForm: FormGroup;
   customer = new Customer();
+  states: Array<Object> = [
+    { label: 'AL', value: 'Alabama' },
+    { label: 'AK', value: 'Alaska' },
+    { label: 'AZ', value: 'Arizona' },
+    { label: 'AR', value: 'Arkansas' },
+    { label: 'CA', value: 'California' },
+    { label: 'CO', value: 'Colorado' },
+    { label: 'WI', value: 'Wisconsin' },
+    { label: 'WY', value: 'Wyoming' },
+  ];
 
-  constructor() { }
+  constructor(private fb: FormBuilder) { }
 
   ngOnInit() {
-    this.customerFrom = new FormGroup({
-      firstName: new FormControl(),
-      lastName: new FormControl(),
-      email: new FormControl(),
-      sendCatalog: new FormControl(true)
+    this.customerForm =  this.fb.group({
+      firstName: '',
+      lastName: '',
+      email: '',
+      sendCatalog: true
     });
   }
 
   save() {
-    console.log(this.customerFrom);
-    console.log('Saved: ' + JSON.stringify(this.customerFrom.value));
+    console.log(this.customerForm);
+    console.log('Saved: ' + JSON.stringify(this.customerForm.value));
   }
 }
