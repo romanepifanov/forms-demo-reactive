6c306f0312a1d2c04b907be99266dfaa3fb2462d
Roman Epifanov <roane@softwave.tech>
2019-05-22 22:05:21 +0300

    added new fields and validating for them

--- src/app/customers/customer.component.ts
+++ src/app/customers/customer.component.ts
@@ -1,7 +1,15 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormBuilder } from '@angular/forms';
+import { FormGroup, FormBuilder, Validators, AbstractControl } from '@angular/forms';
 
 import { Customer } from './customer';
+
+// function ratingRange(item: AbstractControl): { [key: string]: boolean } | null {
+//   if (item.value !== null && (isNaN(item.value) || item.value < 1 || item.value > 5)) {
+//     return { 'range': true };
+//   }
+
+//   return null;
+// }
 
 @Component({
   selector: 'app-customer',
@@ -25,10 +33,13 @@ export class CustomerComponent implements OnInit {
   constructor(private fb: FormBuilder) { }
 
   ngOnInit() {
-    this.customerForm =  this.fb.group({
-      firstName: '',
-      lastName: '',
-      email: '',
+    this.customerForm = this.fb.group({
+      firstName: ['', [Validators.required, Validators.maxLength(3)]],
+      lastName: ['', [Validators.required, Validators.maxLength(50)]],
+      email: ['', [Validators.required, Validators.email]],
+      phone: '',
+      notification: 'email',
+      rating: [null, [Validators.min(1), Validators.max(5)]],
       sendCatalog: true
     });
   }
@@ -36,5 +47,11 @@ export class CustomerComponent implements OnInit {
   save() {
     console.log(this.customerForm);
     console.log('Saved: ' + JSON.stringify(this.customerForm.value));
+  }
+
+  SetNotification(value: string): void {
+    const control = this.customerForm.get('phone');
+    value === 'text' ? control.setValidators(Validators.required) : control.clearAsyncValidators();
+    control.updateValueAndValidity();
   }
 }
